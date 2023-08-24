d66434d7c8610be73f068fa4e79860764ba21524
Vasilyuriy <yur.vasylenko@gmail.com>
2023-08-24 23:03:46 +0300

    pre-finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+const container = document.querySelector(".gallery");
+console.log(container);
+console.log(galleryItems);
+
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        ` <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+container.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+container.addEventListener("click", handlerProductClick);
+
+function handlerProductClick(evt) {
+  evt.preventDefault();
+  let galleryBox = new SimpleLightbox(".gallery a");
+  gallery.on("show.simplelightbox", function () {});
+}
